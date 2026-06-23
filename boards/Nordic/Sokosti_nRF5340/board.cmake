0a13ccf13a78e47bea0c7a375689c18cab2658c1
riderson-1 <royandersondd@proton.me>
2026-06-23 17:31:47 +0300

    first commit with working blinky board hw

---
+++ boards/Nordic/Sokosti_nRF5340/board.cmake
@@ -0,0 +1,17 @@
+if(CONFIG_TRUSTED_EXECUTION_NONSECURE)
+  set(TFM_PUBLIC_KEY_FORMAT "full")
+endif()
+
+if(CONFIG_TFM_FLASH_MERGED_BINARY)
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
+endif()
+
+if(SOC_NRF5340_CPUAPP)
+  board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
+elseif(SOC_NRF5340_CPUNET)
+  board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
+endif()
+
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
